96b0afb0d376b2bf3e0d94ecea908bccf3321f47
thisisanro <antonromanenko@tutanota.com>
2025-08-22 17:26:49 +0000

    add basic repl functionality

--- repl.go
+++ repl.go
@@ -1,6 +1,25 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		command := input[0]
+		fmt.Printf("Your command was: %s\n", command)
+	}
+}
 
 // split user's input by spaces into slice of lowercase strings
 func cleanInput(text string) []string {
